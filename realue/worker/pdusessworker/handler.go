1b96757165bb029e942ae75964486a01a523f38a
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2022-11-24 10:24:42 +0100

    style(golang-lint): fixed a few errcheck errors, one I removed the return statement for a goroutine
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- realue/worker/pdusessworker/handler.go
+++ realue/worker/pdusessworker/handler.go
@@ -113,7 +113,10 @@ func HandleIcmpMessage(pduSess *realuectx.PduSession,
 		pduSess.RxDataPktCount++
 		if pduSess.ReqDataPktInt == 0 {
 			if pduSess.TxDataPktCount < pduSess.ReqDataPktCount {
-				SendIcmpEchoRequest(pduSess)
+				err = SendIcmpEchoRequest(pduSess)
+				if err != nil {
+					return fmt.Errorf("failed to send icmp echo req:%v", err)
+				}
 			} else {
 				msg := &common.UuMessage{}
 				msg.Event = common.DATA_PKT_GEN_SUCCESS_EVENT
@@ -175,11 +178,11 @@ func HandleDataPktGenRequestEvent(pduSess *realuectx.PduSession,
 			return fmt.Errorf("failed to send icmp echo req:%v", err)
 		}
 	} else {
-		go func(pduSess *realuectx.PduSession) error {
+		go func(pduSess *realuectx.PduSession) {
 			for pduSess.TxDataPktCount < pduSess.ReqDataPktCount {
 				err = SendIcmpEchoRequest(pduSess)
 				if err != nil {
-					return fmt.Errorf("failed to send icmp echo req:%v", err)
+					pduSess.Log.Errorln("failed to send icmp echo req")
 				}
 				time.Sleep(time.Duration(pduSess.ReqDataPktInt) * time.Second)
 			}
@@ -187,7 +190,6 @@ func HandleDataPktGenRequestEvent(pduSess *realuectx.PduSession,
 			msg.Event = common.DATA_PKT_GEN_SUCCESS_EVENT
 			pduSess.WriteUeChan <- msg
 			pduSess.Log.Traceln("Sent Data Packet Generation Success Event")
-			return nil
 		}(pduSess)
 	}
 	return nil
